50706c1466815cddac4bfb299e79e71cad03dc0e
Alex Velasquez <jtinnesco@gmail.com>
2019-04-23 05:24:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ def countdown(x)
     return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(num) 
-  while num > 0
-    puts "#{num} SECOND(S)!"
-    num -= 1
-    sleep(5)
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep(1)
   end
     return "HAPPY NEW YEAR!"
 end
